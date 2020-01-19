12dc0deccd5532a4505b564a778a3653fe2f333a
Noah Rose Ledesma <roseledesma@ucdavis.edu>
2020-01-18 17:08:18 -0800

    Add testing stuff

--- src/HelloWorld.ts
+++ src/HelloWorld.ts
@@ -1,2 +1,13 @@
-let my_world : string = "Sarah"
-console.log(`Hello ${my_world}`);
+var AYLIENTextAPI = require('aylien_textapi');
+var textapi = new AYLIENTextAPI({
+  application_id: "334c2982",
+  application_key: "bffdab09177b1c067670cbf31e705be1"
+});
+
+textapi.sentiment({
+    'text': 'John is a very good football player!'
+  }, function(error: any, response: any) {
+    if (error === null) {
+      console.log(response);
+    }
+  });
